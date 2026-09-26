0287d1b84ab3d74419f74d491b13b2b69983e536
Bibhu Prasad <90965874+Void24x@users.noreply.github.com>
2026-09-27 00:38:04 +0530

    Add: valid-tree - submission-0

---
+++ Data Structures & Algorithms/valid-tree/submission-0.py
@@ -0,0 +1,35 @@
+class Solution:
+    def dfs(self, node, vis, adjL, parent):
+        vis[node] = 1
+
+        for it in adjL[node]:
+            if not vis[it]:
+                self.dfs(it, vis, adjL, node)
+            elif it != parent:
+                return True
+        return False
+
+
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        adjL=[[] for _ in range(n)]
+        vis = [0] * n
+        #Create adj list
+        for u,v in edges:
+            adjL[u].append(v)
+            adjL[v].append(u)
+        #do dfs traversal
+        comp = 0
+        
+        for i in range(0,n):
+            if not vis[i]:
+                comp += 1
+                isCycle = self.dfs(i, vis, adjL, -1)
+         
+        if comp == 1 and isCycle == False:
+            return True
+        return False
+           
+
+
+        
+        
